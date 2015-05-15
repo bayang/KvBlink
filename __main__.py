fa6bd878f611e0b8315918662477fd0cfbb353e1
bayang <bayang@users.noreply.github.com>
2015-05-16 00:20:39 +0200

    TODO:timer stops at 1

--- __main__.py
+++ __main__.py
@@ -7,7 +7,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
-from kivy.properties import BooleanProperty, NumericProperty
+from kivy.properties import BooleanProperty, NumericProperty, StringProperty
 from kivy.utils import get_random_color
 from random import random, choice
 import time
@@ -30,6 +30,9 @@ class BlinkButton(Button):
 
 
 class BlinkGame(Screen):
+    TIMER = NumericProperty(20)
+    game_is_running = BooleanProperty(False)
+
     def __init__(self, **kwargs):
         super(BlinkGame, self).__init__(**kwargs)
 
@@ -45,17 +48,36 @@ class BlinkGame(Screen):
         Clock.schedule_once(restore_color, 0.4)
 
     def startgame(self):
-        game_is_running = False
-        if not game_is_running:
+        if not self.game_is_running:
+            self.game_is_running = True
+            myapp = App.get_running_app()
+            self.TIMER = 20
+            myapp.SCORE = 0
             Clock.schedule_interval(self.set_color, 0.5)
-            game_is_running = True
             Clock.schedule_once(self.stopgame, 20)
+            Clock.schedule_interval(self.updatetimer, 1)
         else:
             pass
 
     def stopgame(self, *args):
         Clock.unschedule(self.set_color)
+        Clock.unschedule(self.updatetimer)
+        self.game_is_running = False
 
+    def updatetimer(self, TIMER):
+        if self.TIMER >= 0:
+            self.TIMER -= 1
+        else:
+            self.TIMER = 0
+            self.stopgame()
+            myapp = App.get_running_app()
+            box = BoxLayout()
+            mybutton = Button(text='Close me !')
+            box.add_widget(Label(text='Time over, your score is {}'.format(myapp.SCORE)))
+            box.add_widget(mybutton)
+            popup = Popup(title='BlinkGame', content=box, size_hint=(0.5, 0.5))
+            mybutton.bind(on_press = popup.dismiss)
+            popup.open()
 
 class BlinkApp(App):
     SCORE = NumericProperty(0)
