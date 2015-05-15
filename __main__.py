541e65ea151b5189af60444e8a1c5fc99a17728a
bayang <bayang@users.noreply.github.com>
2015-05-15 15:58:45 +0200

    ajout 2e ecran

--- __main__.py
+++ __main__.py
@@ -4,36 +4,61 @@ from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import BooleanProperty
 from random import random, choice
 import time
 
-from kivy.clock import Clock
 
-class BlinkGame(BoxLayout):
+class Homescreen(Screen):
+    pass
+
+
+class BlinkButton(Button):
+    blinking = BooleanProperty(False)
+    def win_or_not(self):
+        if self.blinking:
+            print("you win")
+        else:
+            print("try again")
+
+
+class BlinkGame(Screen):
     def __init__(self, **kwargs):
         super(BlinkGame, self).__init__(**kwargs)
 
-
     def set_color(self, *args):
 
-        rdid=choice([key for key in self.ids.keys()])
+        rdid = choice([key for key in self.ids.keys()])
+        self.ids[rdid].background_color = (0, 1, 1, 1)
+        self.ids[rdid].blinking = True
 
-        self.ids[rdid].background_color= (0,1,1,1)
         def restore_color(*args):
-            self.ids[rdid].background_color=(1,0,1,1)
-        Clock.schedule_once(restore_color, 0.2)
+            self.ids[rdid].background_color = (1, 0, 1, 1)
+            self.ids[rdid].blinking = False
+        Clock.schedule_once(restore_color, 0.4)
 
     def startgame(self):
-        Clock.schedule_interval(self.set_color, 0.5)
+        game_is_running = False
+        if not game_is_running:
+            Clock.schedule_interval(self.set_color, 0.5)
+            game_is_running = True
+        else:
+            pass
+
+    def stopgame(self):
+        Clock.unschedule(self.set_color)
+
 
 class BlinkApp(App):
     def build(self):
-        game= BlinkGame()
-        game.startgame()
-        #print(game.ids.bu7.background_color)
+        #print(game.)
         #print([key for key in game.ids.keys()])
         #print(choice([key for key in game.ids.keys()]))
-        return game
+        sm = ScreenManager()
+        sm.add_widget(Homescreen(name='home'))
+        sm.add_widget(BlinkGame(name='game'))
+        return sm
 
 
 if __name__ == "__main__":
